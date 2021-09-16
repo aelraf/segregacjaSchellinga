62dc33575ab7cc9b2c6d50a003a216c7c1b0312b
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-09-16 11:07:31 +0200

    poprawki automatycznie wygenerowanej klasy main

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# RafKac
+# 2021_09_16
+# algorytm powstawania getta, czyli algorytm segregacji Schellinga ("Schelling model segregation")
+
+
+
+
+if __name__ == '__main__':
+    print("Cześć")
